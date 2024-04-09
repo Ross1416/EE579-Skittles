fde72b38054aee42fac0dc850229d0341ff3a811
Ross1416 <r.inglis617@gmail.com>
2024-04-09 16:32:27 +0100

    Infrared Functionality updated
    Renamed functions, removed debugging LEDs, allowed port selection and fixed small errors

--- CarReorganised/Infrared.c
+++ CarReorganised/Infrared.c
@@ -2,7 +2,7 @@
 Filename    : CarReorganised/Infrared.c
 Author      : Andrew Law
 Project     : EE579 Skittles Knocking Over Project
-Date        : 02/2/24
+Date        : 02/02/24
 Description : Infrared Sensor Functionality
 --------------------------------------------------------------------------------
 Change History
@@ -10,47 +10,51 @@ Change History
 02-FEB-2024 andrewlaw9178 created to implement functionality of Infrared code
 12-FEB-2024 andrewlaw9178 created to read digital output through ADC
 03-MAR-2024 andrewlaw9178 created to fix issues to allow use of digital output
-08-MAR-2024 andrewlaw9178 created to
 01-MAR-2024 andrewlaw9178 created to provide structure to the infrared code
-03-APR-2024 andrewlaw9178 created to fix stupid error in Infrared code
+03-APR-2024 andrewlaw9178 created to fix error in Infrared code
+09-APR-2024 Ross Inglis updated code to allow different ports selected, renamed
+            functions and removed debugging LEDs.
 --------------------------------------------------------------------------------
 */
+
 #include <msp430.h>
 #include "Infrared.h"
-#define GREEN_LED BIT0
-#define RED_LED BIT6
 
-void irSensorSetup(struct Infrared *infraSense)
+
+void IRSetup(struct Infrared *ir)
 {
-    /* Setup Ports */
-    P1DIR |= (GREEN_LED + RED_LED);                 // GREEN signifies black and RED signifies white
-    P1OUT &= ~(GREEN_LED + RED_LED);                // Turn debugging lights off
-
-    /* Setup Port Interrupt if desired*/
-    P2DIR |= 0x00;
-    P2OUT |= 0x00;
-    P2IES &= ~(infraSense->pin);                        // Low to High - When detect white skittle it triggers response (as need to not hit it)
-    P2IFG &= ~(infraSense->pin);                        // Clears interrupt flag
-    P2IE |= (infraSense->pin);                          // Interrupt enabled for P1
+    if (ir->port == 1)
+    {
+        P1DIR &= ~ir->pin;
+    }
+    else if (ir->port == 2)
+    {
+        P2DIR &= ~ir->pin;
+    }
 }
 
-void read_ir_sensor(struct Infrared *ir)
+void readIR(struct Infrared *ir)
 {
-  if (P2IN & (ir->pin))                         // Black skittle or environemnt detected
-  {
-      ir->colour = 0;                         // 'b' for black
-  }
-  else                                          // White skittle detected
-  {
-      ir->colour = 1;                         // 'w' for white
-  }
-
-  if (ir->colour == 0){
-      P1OUT |= GREEN_LED;                       // Signify black skittle
-      P1OUT &= ~ RED_LED;                       // It is not a white skittle
-  }
-  else{
-      P1OUT &= ~ GREEN_LED;                     // It is not a black skittle
-      P1OUT |= RED_LED;                         // Signify white skittle
-  }
+    if (ir->port == 1)
+    {
+      if (P1IN & (ir->pin))                         // Black skittle or environemnt detected
+      {
+          ir->colour = 0;                         // 'b' for black
+      }
+      else                                          // White skittle detected
+      {
+          ir->colour = 1;                         // 'w' for white
+      }
+    }
+    else if (ir->port == 2)
+    {
+        if (P2IN & (ir->pin))                         // Black skittle or environment detected
+        {
+            ir->colour = 0;                         // 1 for black
+        }
+        else                                          // White skittle detected
+        {
+            ir->colour = 1;                         // 1 for white
+        }
+    }
 }
--- CarReorganised/Infrared.h
+++ CarReorganised/Infrared.h
@@ -2,14 +2,15 @@
 Filename    : CarReorganised/Infrared.h
 Author      : Andrew Law
 Project     : EE579 Skittles Knocking Over Project
-Date        : 01/4/24
+Date        : 01/04/24
 Description : Header file for Infrared Sensor Functionality
 --------------------------------------------------------------------------------
 Change History
 --------------------------------------------------------------------------------
 01-APR-2024 andrewlaw9178 created to have a structured system for Infrared code
 02-APR-2024 andrewlaw9178 created to fix and refine header file of Infrared Sensor
-03-APR-2024 andrewlaw9178 created to fix stupid error in header file of Infrared code
+03-APR-2024 andrewlaw9178 created to fix  error in header file of Infrared code
+09-APR-2024 Ross Inglis updated with renamed functions
 --------------------------------------------------------------------------------
 */
 #ifndef INFRARED_H
@@ -18,12 +19,12 @@ Change History
 //  Define variable containing infrared sensor information
 struct Infrared{
   char colour;
-  int pin;
+  char pin;
+  char port;
 };
 
-
-extern void read_ir_sensor();
-extern void irSensorSetup(struct Infrared *ir1);
+extern void IRSetup(struct Infrared *ir);
+extern void readIR(struct Infrared *ir);
 
 #endif // INFRARED_H
 //==============================================================================
