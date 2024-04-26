11021f6ee1b25949ff2cdb0cfe3ca24c3c6b8849
Kandor9178 <andrew.s.law@gmail.com>
2024-04-26 20:01:03 +0100

    Tests into one project with single main control file to select tests
    (Has indicator LED, start button, drive and steer tests)

---
+++ HardwareTests/HardwareTests/Debug/subdir_vars.mk
@@ -0,0 +1,53 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430g2553.cmd 
+
+C_SRCS += \
+../Button.c \
+../Common.c \
+../DCMotor.c \
+../IndicatorLED.c \
+../main.c 
+
+C_DEPS += \
+./Button.d \
+./Common.d \
+./DCMotor.d \
+./IndicatorLED.d \
+./main.d 
+
+OBJS += \
+./Button.obj \
+./Common.obj \
+./DCMotor.obj \
+./IndicatorLED.obj \
+./main.obj 
+
+OBJS__QUOTED += \
+"Button.obj" \
+"Common.obj" \
+"DCMotor.obj" \
+"IndicatorLED.obj" \
+"main.obj" 
+
+C_DEPS__QUOTED += \
+"Button.d" \
+"Common.d" \
+"DCMotor.d" \
+"IndicatorLED.d" \
+"main.d" 
+
+C_SRCS__QUOTED += \
+"../Button.c" \
+"../Common.c" \
+"../DCMotor.c" \
+"../IndicatorLED.c" \
+"../main.c" 
+
+
