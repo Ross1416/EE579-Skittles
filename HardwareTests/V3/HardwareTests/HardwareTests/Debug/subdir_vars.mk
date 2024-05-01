93f2f1d4ef9541df49886415219712c2b9c60993
Kandor9178 <andrew.s.law@gmail.com>
2024-05-01 18:42:30 +0100

    Updated tests to adhere UserInterface .c/.h
    Next will look at other header files and get slide to work Aswitch

---
+++ HardwareTests/V3/HardwareTests/HardwareTests/Debug/subdir_vars.mk
@@ -0,0 +1,65 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430g2553.cmd 
+
+C_SRCS += \
+../Common.c \
+../DCMotor.c \
+../Infrared.c \
+../Servo.c \
+../Ultrasonic.c \
+../UserInterface.c \
+../main.c 
+
+C_DEPS += \
+./Common.d \
+./DCMotor.d \
+./Infrared.d \
+./Servo.d \
+./Ultrasonic.d \
+./UserInterface.d \
+./main.d 
+
+OBJS += \
+./Common.obj \
+./DCMotor.obj \
+./Infrared.obj \
+./Servo.obj \
+./Ultrasonic.obj \
+./UserInterface.obj \
+./main.obj 
+
+OBJS__QUOTED += \
+"Common.obj" \
+"DCMotor.obj" \
+"Infrared.obj" \
+"Servo.obj" \
+"Ultrasonic.obj" \
+"UserInterface.obj" \
+"main.obj" 
+
+C_DEPS__QUOTED += \
+"Common.d" \
+"DCMotor.d" \
+"Infrared.d" \
+"Servo.d" \
+"Ultrasonic.d" \
+"UserInterface.d" \
+"main.d" 
+
+C_SRCS__QUOTED += \
+"../Common.c" \
+"../DCMotor.c" \
+"../Infrared.c" \
+"../Servo.c" \
+"../Ultrasonic.c" \
+"../UserInterface.c" \
+"../main.c" 
+
+
